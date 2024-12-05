2884acafd9c7661be5570251081f45c760665c66
Andrey Gerasimov <81647354+FireGoose@users.noreply.github.com>
2024-12-05 09:39:32 +0300

    Update task1.c (Герасимов Андрей Алексеевич)

--- 3824Б1ФИ1/08 Герасимов Андрей Алексеевич/task1.c
+++ 3824Б1ФИ1/08 Герасимов Андрей Алексеевич/task1.c
@@ -3,5 +3,11 @@
 // Если a + b <= INT_MIN, возвращать INT_MIN
 int sum_of_two_numbers(int a, int b)
 {
-  return 0;
+  long long s = a + b;
+  if (s >= INT_MAX)
+    return INT_MAX;
+  else if (s <= INT_MIN)
+    return INT_MIN;
+  else
+    return a + b;
 }
